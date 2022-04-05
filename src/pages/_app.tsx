b2d43ec81855d67c1fb31998da3aa280b983a8f3
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-05 23:33:17 +0900

    add NextPageWithLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import type { AppProps } from 'next/app'
-import React from 'react'
 import 'src/styles/tailwind.css'
+import type { AppPropsWithLayout } from 'next/app'
+import React from 'react'
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
