289bc48fa421c69274f68ec21178d44b4204b98b
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-11 16:22:55 +0900

    add keys

--- src/components/blocks/blockSchema.tsx
+++ src/components/blocks/blockSchema.tsx
@@ -9,30 +9,34 @@ import { BlockSchema, defaultBlockSchema } from '~/src/lib/ntn'
 export const blockSchema: BlockSchema = {
   ...defaultBlockSchema,
   paragraph: {
-    render: (block) => <Paragraph block={block} />,
+    render: (block) => <Paragraph block={block} key={block.id} />,
   },
   column: {
     render: (block, renderBlocks) => (
-      <Column block={block} renderBlocks={renderBlocks} />
+      <Column block={block} renderBlocks={renderBlocks} key={block.id} />
     ),
   },
   column_list: {
     render: (block, renderBlocks) => (
-      <ColumnList block={block} renderBlocks={renderBlocks} />
+      <ColumnList block={block} renderBlocks={renderBlocks} key={block.id} />
     ),
   },
   heading_1: {
-    render: (block) => <Heading1 block={block} />,
+    render: (block) => <Heading1 block={block} key={block.id} />,
   },
   heading_2: {
-    render: (block) => <Heading2 block={block} />,
+    render: (block) => <Heading2 block={block} key={block.id} />,
   },
   heading_3: {
-    render: (block) => <Heading3 block={block} />,
+    render: (block) => <Heading3 block={block} key={block.id} />,
   },
   bulleted_list_item: {
     render: (block, renderBlocks) => (
-      <BulletedListItem block={block} renderBlocks={renderBlocks} />
+      <BulletedListItem
+        block={block}
+        renderBlocks={renderBlocks}
+        key={block.id}
+      />
     ),
   },
 }
