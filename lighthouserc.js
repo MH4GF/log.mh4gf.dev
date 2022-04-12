006b4e74e5d60ec2b9c8eaa586890be5c5d0dc61
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-12 18:07:49 +0900

    disable assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,6 +8,16 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
+      // TODO: enable ignored assertions
+      assertions: {
+        'csp-xss': 'off',
+        'errors-in-console': 'off',
+        'installable-manifest': 'off',
+        'maskable-icon': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+      },
     },
   },
 }
