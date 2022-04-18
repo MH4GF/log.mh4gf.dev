380f57316861e5b8f03be97fa635ac5654a642d6
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-18 23:13:14 +0900

    configure default background color of storybook
    ref: https://storybook.js.org/docs/react/essentials/backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,4 +16,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#111827',
+      },
+    ],
+  },
 }
