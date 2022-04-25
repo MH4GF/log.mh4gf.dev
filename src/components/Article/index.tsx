5500d20a48b574201af525da0e9fb27ffdbef4f5
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-25 23:07:35 +0900

    tweak article page

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -13,9 +13,10 @@ export const Article: FC<Props> = ({ article }) => {
   const { renderBlocks } = useRenderBlocks(blockRenderRules)
 
   return (
-    <article className="max-w-2xl mx-auto">
-      {article.title}
-      {renderBlocks(article.blocks)}
+    <article className="mx-auto max-w-2xl">
+      <h1 className="text-4xl font-bold text-white">{article.title}</h1>
+      {/* TODO: タグ */}
+      <div className="mt-6 grid gap-4">{renderBlocks(article.blocks)}</div>
     </article>
   )
 }
