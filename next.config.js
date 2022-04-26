8b51df8de118dc64ef5b8fea33331e18f35893e3
MH4GF <h.miyagi.cnw@gmail.com>
2022-04-26 20:50:03 +0900

    add webpack config to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // https://www.npmjs.com/package/@svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
