df6f54f6873276ed47d95456df33e4df9622c0f4
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-09 16:49:21 +0900

    wrap about link for copylight

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,15 @@
-export const Footer = () => {
-  return <footer>© Hirotaka Miyagi</footer>
+import { ComponentPropsWithoutRef, FC } from 'react'
+
+import { externalLinks } from '~/src/utils/externalLinks'
+
+export const Footer: FC<ComponentPropsWithoutRef<'footer'>> = (props) => {
+  return (
+    <footer {...props}>
+      <div>
+        <a href={externalLinks.about} className="underline">
+          © Hirotaka Miyagi
+        </a>
+      </div>
+    </footer>
+  )
 }
