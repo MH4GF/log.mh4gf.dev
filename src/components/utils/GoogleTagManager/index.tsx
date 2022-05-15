fe1977ba66e5722af90c217032958e83bf6e999b
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-15 15:14:17 +0900

    add google tag manager

---
+++ src/components/utils/GoogleTagManager/index.tsx
@@ -0,0 +1,26 @@
+import Script from 'next/script'
+import { FC } from 'react'
+
+export type GoogleTagManaagerId = `GTM-${string}`
+
+type Props = {
+  gtmId: GoogleTagManaagerId
+}
+
+export const GoogleTagManager: FC<Props> = ({ gtmId }) => {
+  return (
+    <Script
+      id="google-tag-manager"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${gtmId}');
+      `,
+      }}
+    />
+  )
+}
