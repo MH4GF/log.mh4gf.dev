0c08ffa29a2c967bf659ece92aee210218b19df8
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-19 10:22:45 +0900

    add padding to code text

--- src/components/blocks/RichText/index.tsx
+++ src/components/blocks/RichText/index.tsx
@@ -16,7 +16,7 @@ const classNames = (annotations: RichTextObject['annotations']) => {
   const classes = ['text-white']
 
   if (annotations.underline) classes.push(...classes, underlineClass)
-  if (annotations.code) classes.push(...classes, 'rounded bg-gray-500')
+  if (annotations.code) classes.push(...classes, 'rounded bg-gray-500 px-1')
 
   return classes.join(' ')
 }
