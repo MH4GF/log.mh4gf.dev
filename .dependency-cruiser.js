7ab230acd0b968ecb21f2e84ccf35f14e0eafc5b
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-19 11:27:15 +0900

    exclude ntn dir for depcruiser

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -194,10 +194,7 @@ module.exports = {
        - dynamic: a boolean indicating whether to ignore dynamic (true) or static (false) dependencies.
           leave out if you want to exclude neither (recommended!)
     */
-    // exclude : {
-    //   path: '',
-    //   dynamic: true
-    // },
+    exclude: ['src/lib/ntn'],
 
     /* pattern specifying which files to include (regular expression)
        dependency-cruiser will skip everything not matching this pattern
