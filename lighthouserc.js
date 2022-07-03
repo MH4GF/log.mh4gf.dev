cfaf6d7d492e486dec1956954e102f0345c8abf4
Hirotaka Miyagi <31152321+MH4GF@users.noreply.github.com>
2022-07-04 08:15:14 +0900

    Revert "disable lighthouse ci assertion"

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,5 +6,18 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+      // TODO: enable ignored assertions
+      assertions: {
+        'csp-xss': 'off',
+        'errors-in-console': 'off',
+        'installable-manifest': 'off',
+        'maskable-icon': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+      },
+    },
   },
 }
