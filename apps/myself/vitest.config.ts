0000627d30928f2253551bf66d22b598ee9a7065
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-21 19:30:31 +0900

    build(dpes): add vitest

---
+++ apps/myself/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${__dirname}/`,
+    },
+  },
+  test: {
+    globals: true,
+  },
+})
