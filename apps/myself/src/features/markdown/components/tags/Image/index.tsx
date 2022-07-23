f40aec75ea763c9ab2eb4d380549f607f0020d5b
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-23 16:03:45 +0900

    feat(myself): render image component

---
+++ apps/myself/src/features/markdown/components/tags/Image/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import type { ComponentPropsWithoutRef, FC } from 'react'
+
+type Props = ComponentPropsWithoutRef<'img'>
+
+const ImageComponent: FC<Props> = (props) => {
+  const { src, alt } = props
+  return <Image src={src || ''} alt={alt} width="500" height="500" />
+}
+
+export { ImageComponent as Image }
--- apps/myself/src/features/markdown/processor.ts
+++ apps/myself/src/features/markdown/processor.ts
@@ -4,6 +4,8 @@ import remarkParse from 'remark-parse'
 import remark2Rehype from 'remark-rehype'
 import { unified } from 'unified'
 
+import { Image } from './components/tags'
+
 export const processor = unified()
   .use(remarkParse)
   .use(remark2Rehype)
@@ -12,5 +14,6 @@ export const processor = unified()
     Fragment,
     components: {
       h1: 'h1',
+      img: Image,
     },
   })
