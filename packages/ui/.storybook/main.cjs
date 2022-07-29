17c35cded585d7be2472ae6130bad5a5f08753ae
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-29 10:02:23 +0900

    build: setup storybook for ui package

---
+++ packages/ui/.storybook/main.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../lib/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+}
