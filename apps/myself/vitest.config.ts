e09f633beb7b5e01d975a4fb71c184c7ab24a288
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-29 09:28:10 +0900

    fix: ts-ignore

--- apps/myself/vitest.config.ts
+++ apps/myself/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     react({
       babel: {
         plugins: ['@vanilla-extract/babel-plugin'],
