c788d0643d319c807a19416bb7924f72cd14d0a0
MH4GF <h.miyagi.cnw@gmail.com>
2022-08-15 08:36:26 +0900

    fix(myself): remove paragraph if nested anchor link

--- apps/myself/src/features/markdown/features/link/plugins/dropParagraph.test.ts
+++ apps/myself/src/features/markdown/features/link/plugins/dropParagraph.test.ts
@@ -3,15 +3,15 @@ import { unified } from 'unified'
 import { u } from 'unist-builder'
 import { removePosition } from 'unist-util-remove-position'
 
-import { transformLinkCard } from './transformLinkCard'
+import { dropParagraph } from './dropParagraph'
 
-describe('transformLinkCard', () => {
+describe('dropParagraph', () => {
   it('transform to linkCard from paragraph', () => {
-    const processor = unified().use(remarkParse).use(transformLinkCard)
+    const processor = unified().use(remarkParse).use(dropParagraph)
     const parsed = processor.parse(`[link](https://example.com)`)
     const result = removePosition(processor.runSync(parsed), true)
     const expected = u('root', [
-      u('linkCard', [u('link', { title: null, url: 'https://example.com' }, [u('text', 'link')])]),
+      u('link', { title: null, url: 'https://example.com' }, [u('text', 'link')]),
     ])
 
     expect(result).toStrictEqual(expected)
