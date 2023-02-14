ca3aa523614859b600b7a1f430a8f05eaf53edde
MH4GF <h.miyagi.cnw@gmail.com>
2023-02-15 08:33:04 +0900

    fix: remove about app

--- apps/about/src/content/config.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import { defineCollection } from 'astro:content'
-const meCollection = defineCollection({})
-export const collections = {
-  me: meCollection,
-}
