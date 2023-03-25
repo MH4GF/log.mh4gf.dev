cc895fe2234af5428d81c7e684bc36472cc19ecd
MH4GF <h.miyagi.cnw@gmail.com>
2023-03-25 16:29:29 +0900

    fix: add optimizedDeps

--- apps/me/astro.config.mjs
+++ apps/me/astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import remarkToc from 'remark-toc'
-import remarkCollapse from 'remark-collapse'
-import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import remarkCollapse from 'remark-collapse'
+import remarkToc from 'remark-toc'
 
 // https://astro.build/config
 export default defineConfig({
@@ -34,5 +34,13 @@ export default defineConfig({
       wrap: true,
     },
     extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: [
+        // https://stackoverflow.com/questions/75640753/vite-esbuild-error-no-loader-is-configured-for-node-files-node-modules-fs
+        'fsevents',
+      ],
+    },
   },
 })
